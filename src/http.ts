ec8811dc5a38521abbceb29bf0f4b5fc3f4d9644
dspindola <noturno@sent.com>
2024-07-08 22:16:04 +0000

    refactor server http module
    move base server setup related to plugins and base hooks to src/setup.ts

--- src/http.ts
+++ src/http.ts
@@ -1,44 +1,5 @@
-import { Elysia } from "elysia"
-import { html } from "@elysiajs/html"
-import { cors } from "@elysiajs/cors"
-import { swagger } from "@elysiajs/swagger"
-import { staticPlugin } from "@elysiajs/static"
-import logixlysia from 'logixlysia'
-import { elysiaObjectStoragePlugin } from "./plugins/storage"
-import { get, } from "./routes";
+import { get } from "./routes";
+import { setup } from "./setup";
 
-const mode = process.env.NODE_ENV !== "production" ? 'dev' : 'prod'
-
-const http = new Elysia()
-	.use(logixlysia({
-		config: {
-			ip: true,
-			logFilePath: `${process.cwd()}/.tmp/.logs/${mode}.log`,
-			customLogFormat:
-				'🦊 {now} {level} {duration} {method} {pathname} {status} {message} {ip}',
-			logFilter: {
-				level: ['INFO', 'ERROR', 'WARNING'],
-				status: [500, 404, 200],
-				method: ['GET', 'POST']
-			}
-		}
-	}))
-	.use(cors())
-	.use(html({
-		autoDetect: true,
-		autoDoctype: true
-	}))
-	.use(staticPlugin({
-		prefix: "/",
-		noCache: true,
-	}))
-	.use(swagger())
-	.use(elysiaObjectStoragePlugin<'/storage'>({
-		prefix: "/storage",
-		bucketId: "replit-objstore-090156f4-ff5a-4431-b7aa-c6ed330d5efb"
-	}))
+export const http = setup
 	.get('/', get)
-
-
-
-export default http;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import http from "./http";
+import { http } from "./http";
+import { mode } from "./setup";
 
 const app = http
 	.listen({
 		port: process.env.PORT,
 		hostname: process.env.HOSTNAME,
-		development: process.env.NODE_ENV !== "production",
+		development: mode === "dev",
 		lowMemoryMode: process.env.LOW_MEMORY_MODE === "1",
 		id: "server",
 		reusePort: process.env.REUSE_PORT === "1",
